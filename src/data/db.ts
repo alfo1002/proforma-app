b86e31089354319c6baae95ba9b5c506494ada31
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-08-16 17:08:25 -0500

    add: agregando types

---
+++ src/data/db.ts
@@ -0,0 +1,154 @@
+import type { MenuItem } from "../types";
+
+export const menuItems: MenuItem[] = [
+    {
+        id: 1,
+        name: "Fundas de Pegamento",
+        price: 7.25
+    },
+    {
+        id: 2,
+        name: "Bolsas de Cemento",
+        price: 6.50
+    },
+    {
+        id: 3,
+        name: "Varillas",
+        price: 6.75
+    },
+    {
+        id: 4,
+        name: "Bloques",
+        price: 1.25
+    },
+    {
+        id: 5,
+        name: "Arena",
+        price: 1.50
+    },
+    {
+        id: 6,
+        name: "Grava",
+        price: 1.75
+    },
+    {
+        id: 7,
+        name: "Piedra",
+        price: 1.75
+    },
+    {
+        id: 8,
+        name: "Hierro",
+        price: 2.00
+    },
+    {
+        id: 9,
+        name: "Madera",
+        price: 1.00
+    },
+    {
+        id: 10,
+        name: "Pintura",
+        price: 2.50
+    },
+    {
+        id: 11,
+        name: "Martillo",
+        price: 2.00
+    },
+    {
+        id: 12,
+        name: "Clavos",
+        price: 1.50
+    },
+    {
+        id: 13,
+        name: "Tornillos",
+        price: 1.75
+    },
+    {
+        id: 14,
+        name: "Tuercas",
+        price: 1.75
+    },
+    {
+        id: 15,
+        name: "Taladro",
+        price: 2.00
+    },
+    {
+        id: 16,
+        name: "Sierra",
+        price: 1.00
+    },
+    {
+        id: 17,
+        name: "Cinta Métrica",
+        price: 2.50
+    },
+    {
+        id: 18,
+        name: "Nivel",
+        price: 2.00
+    },
+    {
+        id: 19,
+        name: "Escuadra",
+        price: 1.50
+    },
+    {
+        id: 20,
+        name: "Pala",
+        price: 1.75
+    },
+    {
+        id: 21,
+        name: "Pico",
+        price: 1.75
+    },
+    {
+        id: 22,
+        name: "Pala de Punta",
+        price: 2.00
+    },
+    {
+        id: 23,
+        name: "Pico de Pato",
+        price: 1.00
+    },
+    {
+        id: 24,
+        name: "Guantes",
+        price: 2.50
+    },
+    {
+        id: 25,
+        name: "Botas",
+        price: 2.00
+    },
+    {
+        id: 26,
+        name: "Casco",
+        price: 1.50
+    },
+    {
+        id: 27,
+        name: "Gafas",
+        price: 1.75
+    },
+    {
+        id: 28,
+        name: "Chaleco",
+        price: 1.75
+    },
+    {
+        id: 29,
+        name: "Cartera de Herramientas",
+        price: 2.00
+    },
+    {
+        id: 30,
+        name: "Caja de Herramientas",
+        price: 1.00
+    }
+];
